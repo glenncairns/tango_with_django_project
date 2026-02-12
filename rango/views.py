ded7394d9b17bfda5300d8fbf895db92a3d83964
“Glenn <3011769C@student.gla.ac.uk>
2026-02-12 23:27:19 +0000

    CHAPTER 10 COMPLETE

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 @login_required
 def user_logout(request):
@@ -18,6 +19,34 @@ def user_logout(request):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
+
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+'last_visit',
+    str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+'%Y-%m-%d %H:%M:%S')
+# If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+# Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+# Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+# Update/set the visits cookie
+    request.session['visits'] = visits
+
+
 
 
 def register(request):
@@ -79,11 +108,21 @@ def index(request):
         'pages': page_list,
     }
 
+    visitor_cookie_handler(request)
+
     return render(request, 'rango/index.html', context=context_dict)
+
+    
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {}
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
+
 
 @login_required
 def add_category(request):
